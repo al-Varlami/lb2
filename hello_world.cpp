9ad27abe688a3ced0d585723f68c9a4d436f5291
Serge <russlandhope@gmail.com>
2025-06-06 18:41:33 +0300

    add comments to hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-#include <string>
+// Это нужно для ввода и вывода
 
 int main() {
     std::string name;
+    // Запрашиваем имя пользователя
     std::cout << "Enter your name: ";
     std::cin >> name;
+    // Выводим персонализированное сообщение
     std::cout << "Hello world from @" << name << std::endl;
     return 0;
 }
