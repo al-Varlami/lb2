15cbd9ec44490cdcf5d695f72ecea90e778d7d95
Serge <russlandhope@gmail.com>
2025-06-06 18:45:01 +0300

    formatted code with clang-format using Mozilla style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    string name;
-    cout << "Enter your name: ";
-    cin >> name;
-    cout << "Hello world from @" << name << endl;
-    return 0;
+int
+main()
+{
+  string name;
+  cout << "Enter your name: ";
+  cin >> name;
+  cout << "Hello world from @" << name << endl;
+  return 0;
 }
